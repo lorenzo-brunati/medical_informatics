e0ca7c2ea194cb02252c829592406e72cc256aa9
lorenzo-brunati <lory.brunati5@gmail.com>
2026-05-11 15:09:58 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+import sqlite3 as sql
+import customtkinter as ctk
+
+class LoginApp():
+    def __init__(self): # Runs only ONCE
+        
+        # Both CONN and CURSOR are created as ATTRIBUTES of the CLASS
+        self.conn = sql.connect("Project/database.db")
+        self.cursor = self.conn.cursor()
+
+        self.root = ctk.CTk() # ROOT (Main Window) in the Hierarchical Structure
+        self.root.title("LoginApp") # Window NAME
+        self.root.geometry("300x300") # Window SIZE (HxV)[Pixel]
+
+        self.setup_gui() # Call the METHOD when OBJ is built
+
+        self.root.mainloop() # Method to RUN Window
+
+
+    def setup_gui(self): # Method of the CLASS
+        # by passing the SELF everything initialized can be retrieved
+        
+        # TEXT LABEL
+        self.login_label = ctk.CTkLabel(self.root, text="Insert Username and Password",
+                                        font=('Arial', 14), width=300, height=30)
+        self.login_label.place(x=0, y=30) # TOP-LEFT coordinate system
+
+        #PASSWORD ENTRY
+        self.pass_entry = ctk.CTkEntry(self.root, placeholder_text="Password",
+                                       font=('Arial', 12), width=200, height=30, show="*")
+        self.pass_entry.place(x=50, y=135)
+
+        # USERNAME ENTRY
+        self.user_entry = ctk.CTkEntry(self.root, placeholder_text="Username",
+                                       font=('Arial', 12), width=200, height=30)
+        self.user_entry.place(x=50, y=90)
+
+        # LOGIN BUTTON
+        self.login_button = ctk.CTkButton(self.root, text="Login",
+                                          font=('Arial', 12), width=100, height=30,
+                                          hover=True, command=self.login_callback)
+        self.login_button.place(x=100, y=180)
+
+        # OUTCOME LABEL
+        self.outcome_label = ctk.CTkLabel(self.root, text="",
+                                          font=('Arial', 12), width=300, height=30)
+        self.outcome_label.place(x=0, y=240)
+
+        self.user_type = None
+        self.tax_code = None
+
+    def login_callback(self):
+        
+        # Retrieve ENTRY Information
+        user = (self.user_entry.get(), )
+        pw = self.pass_entry.get()
+
+        # Execute SQL Query
+        self.cursor.execute("SELECT username FROM user WHERE username = ?", user)
+        result = self.cursor.fetchone()
+        
+        if result is not None: # Existing USERNAME?
+            self.cursor.execute("SELECT password FROM user WHERE username = ?", user)
+            password = self.cursor.fetchone()[0]
+
+            if pw == password:
+                self.outcome_label.configure(text=f"Welcome back {user[0]}!")
+                self.outcome_label.configure(text_color="green")
+
+                self.cursor.execute("SELECT UserType FROM user WHERE username = ?", user)
+                user_type = self.cursor.fetchone()[0]
+
+                if user_type == "Patient":
+                    print("Patient") # patientApp
+                elif user_type == "Doctor":
+                    print("Doctor") # DoctorApp
+                else:
+                    print("Admin") # AdminApp
+
+            else:
+                self.outcome_label.configure(text=f"Wrong Password")
+                self.outcome_label.configure(text_color="red")
+
+        else:
+            self.outcome_label.configure(text="Username doesn't exist.") # UPDATE Label Text
+            self.outcome_label.configure(text_color="red")
+
+LoginApp()
